9b9526a1e5e88699418641cf37e137be77c478f7
Hyungsuk Kang <hskang9@gmail.com>
2023-06-10 13:23:35 +0900

    feat(matchingengine): apply maker/taker fee in contract

---
+++ contracts/safex/fees/FeeTier.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.10;
+
+contract FeeTier {
+    // determine maker and taker
+    // get membership points accrued from accountant
+    // apply fee rate on tier
+}

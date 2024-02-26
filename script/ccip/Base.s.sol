53e04d94e6c092effb125376720ae05783179249
Hyungsuk Kang <hskang9@gmail.com>
2024-02-26 22:23:16 +0900

    feat(CCIP): add ccip tokens in Optimism, Arbitrum, Base

---
+++ script/ccip/Base.s.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {MockBTC} from "../../contracts/mock/MockBTC.sol";
+import {SABT} from "../../contracts/sabt/SABT.sol";
+import {BlockAccountant} from "../../contracts/sabt/BlockAccountant.sol";
+import {Membership} from "../../contracts/sabt/Membership.sol";
+import {Treasury} from "../../contracts/sabt/Treasury.sol";
+import {MockToken} from "../../contracts/mock/MockToken.sol";
+import {MatchingEngine} from "../../contracts/safex/MatchingEngine.sol";
+import {OrderbookFactory} from "../../contracts/safex/orderbooks/OrderbookFactory.sol";
+import {Orderbook} from "../../contracts/safex/orderbooks/Orderbook.sol";
+import {Multicall3} from "../Multicall3.sol";
+import {TokenDispenser} from "../../contracts/safex/airdrops/TokenDispenser.sol";
+import {ExchangeOrderbook} from "../../contracts/safex/libraries/ExchangeOrderbook.sol";
+import {ERC20MintablePausableBurnable} from "../../contracts/stnd/ccip/STND.sol";
+
+contract Deployer is Script {
+    function _setDeployer() internal {
+        uint256 deployerPrivateKey = vm.envUint("LINEA_TESTNET_DEPLOYER_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+    }
+}
+
+contract DeployMulticall3 is Deployer {
+    function run() external {
+        _setDeployer();
+        new Multicall3();
+        vm.stopBroadcast();
+    }
+}
+
+contract DeploySTND is Deployer {
+    ERC20MintablePausableBurnable public stnd;
+    function run() external {
+        _setDeployer();
+        stnd = new ERC20MintablePausableBurnable("Standard (CCIP)", "STND");
+        vm.stopBroadcast();
+    }
+}
+
+contract GrantMinterRole is Deployer {
+    address stnd_address = 0xAd117e349e05c7B718B9AfbFde88EA60376bCE14;
+    ERC20MintablePausableBurnable public stnd;
+    address minter = address(0);
+    function run() external {
+        _setDeployer();
+        stnd = ERC20MintablePausableBurnable(stnd_address);
+        stnd.grantRole(stnd.MINTER_ROLE(), minter);
+    }
+}

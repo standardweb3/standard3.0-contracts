9918cbf9dca8d0cbf0e6150a4f140c0d6fecbf09
Hyungsuk Kang <hskang9@gmail.com>
2024-09-24 14:27:38 +0900

    update STND new token

--- src/stnd/ccip/STND.sol
+++ src/stnd/ccip/STND.sol
@@ -6,41 +6,32 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract ERC20MintablePausableBurnable is
+contract Standard is
     ERC20,
     ERC20Burnable,
-    ERC20Pausable,
     AccessControl
 {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    uint256 private constant MAX_SUPPLY = 20_000_000 * 10**18; // 20 million tokens with 18 decimals
+    error MaxSupplyReached(uint256 currentSupply, uint256 newSupply);
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+
+    constructor() ERC20("Standard", "STND") {
         // Grant the contract deployer the default admin role: they can grant and revoke any roles
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
 
         // Grant the minter and pauser roles to the deployer
         _grantRole(MINTER_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
     }
 
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        if (totalSupply() + amount > MAX_SUPPLY) {
+            revert MaxSupplyReached(totalSupply(), totalSupply() + amount);
+        }
         _mint(to, amount);
     }
 
-    function pause() public onlyRole(PAUSER_ROLE) {
-        _pause();
-    }
-
-    function unpause() public onlyRole(PAUSER_ROLE) {
-        _unpause();
-    }
-
-    function _update(
-        address from,
-        address to,
-        uint256 value
-    ) internal virtual override(ERC20, ERC20Pausable) whenNotPaused {
-        super._update(from, to, value);
+    function remainingMintableSupply() public view returns (uint256) {
+        return MAX_SUPPLY - totalSupply();
     }
 }

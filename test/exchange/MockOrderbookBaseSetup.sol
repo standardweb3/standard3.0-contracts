0e15cac5548affee744851a832cfc3b1daedf79f
Hyungsuk Kang <hskang9@gmail.com>
2025-06-01 19:41:48 +0900

    test on current engine

--- test/exchange/MockOrderbookBaseSetup.sol
+++ test/exchange/MockOrderbookBaseSetup.sol
@@ -8,7 +8,7 @@ import {MockQuote} from "../../src/mock/MockQuote.sol";
 import {MockBTC} from "../../src/mock/MockBTC.sol";
 import {ErrToken} from "../../src/mock/MockTokenOver18Decimals.sol";
 import {Utils} from "../utils/Utils.sol";
-import {MockMatchingEngine} from "../../src/exchange/mocks/MockMatchingEngine.sol";
+import {MatchingEngine} from "../../src/exchange/MatchingEngine.sol";
 import {MockOrderbookFactory} from "../../src/exchange/mocks/MockOrderbookFactory.sol";
 import {MockOrderbook} from "../../src/exchange/mocks/MockOrderbook.sol";
 import {ExchangeOrderbook} from "../../src/exchange/libraries/ExchangeOrderbook.sol";
@@ -17,7 +17,7 @@ import {WETH9} from "../../src/mock/WETH9.sol";
 
 contract MockBaseSetup is Test {
     Utils public utils;
-    MockMatchingEngine public matchingEngine;
+    MatchingEngine public matchingEngine;
     WETH9 public weth;
     MockOrderbookFactory public orderbookFactory;
     MockOrderbook public book;
@@ -55,7 +55,7 @@ contract MockBaseSetup is Test {
         btc.mint(trader2, 10000000e18);
         feeToken = new MockToken("Fee Token", "FEE", 18);
         feeToken.mint(booker, 40000e18);
-        matchingEngine = new MockMatchingEngine();
+        matchingEngine = new MatchingEngine();
         orderbookFactory = new MockOrderbookFactory();
         orderbookFactory.initialize(address(matchingEngine));
         matchingEngine.initialize(address(orderbookFactory), address(booker), address(weth));

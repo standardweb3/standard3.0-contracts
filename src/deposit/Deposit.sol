1da88e20d6f7f72bcdcaf7a88f45ca8ef9b70f3b
Hyungsuk Kang <hskang9@gmail.com>
2025-06-29 22:26:03 +0900

    update Deposit.sol

--- src/deposit/Deposit.sol
+++ src/deposit/Deposit.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.24;
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import {IMatchingEngine} from "../exchange/interfaces/IMatchingEngine.sol";
 
 contract Deposit is AccessControl {
@@ -46,6 +47,7 @@ contract Deposit is AccessControl {
     event DepositETH(address indexed to, uint256 amount);
     event DepositToken(address indexed to, address indexed token, uint256 amount);
     event DepositNFT(address indexed to, address indexed token, uint256 tokenId);
+    event DepositERC1155(address indexed to, address indexed token, uint256 tokenId, uint256 amount);
     event SetSupportedTokens(address[] tokens, bool[] isSupported);
 
     constructor() {
@@ -76,12 +78,20 @@ contract Deposit is AccessControl {
     }
 
     // Deposit NFT to api account
-    function depositNFT(address to, address token, uint256 tokenId) public {
+    function depositERC721(address to, address token, uint256 tokenId) public {
         // send NFT to the API service
         // check if the token is supported
         require(supportedTokens[token], "Token not supported");
         IERC721(token).transferFrom(msg.sender, to, tokenId);
         emit DepositNFT(to, token, tokenId);
+    }
+
+    function depositERC1155(address to, address token, uint256 tokenId, uint256 amount) public {
+        // send ERC1155 to the API service
+        // check if the token is supported
+        require(supportedTokens[token], "Token not supported");
+        IERC1155(token).safeTransferFrom(msg.sender, to, tokenId, amount, "");
+        emit DepositERC1155(to, token, tokenId, amount);
     }
 
     function createOrders(APICreateOrderInput[] memory orders) public payable {
